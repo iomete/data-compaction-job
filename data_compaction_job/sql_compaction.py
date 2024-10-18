61ad8959e035befa69f19cab1e7e2de978e25f2e
alokhp <alokh.p123@gmail.com>
2024-10-18 20:22:41 +0530

    feat: added include exclude and stats tracking for table optimisation

--- data_compaction_job/sql_compaction.py
+++ data_compaction_job/sql_compaction.py
@@ -1,13 +1,14 @@
 import logging
-import time
 import os
+import time
+from collections import defaultdict
+from datetime import datetime, timedelta, timezone
+from functools import cache
+
 import requests
 
-
-from pyspark.sql import SparkSession
-
-from data_compaction_job.config import ApplicationConfig, ExpireSnapshotConfig, RemoveOrphanFilesConfig, \
-    RewriteDataFilesConfig, RewriteManifestsConfig
+from data_compaction_job.config import ApplicationConfig, RewriteManifestsConfig
+from stats_emitter import emit_stats, init_emitter
 
 logger = logging.getLogger(__name__)
 
@@ -23,14 +24,24 @@ class SqlCompaction:
     def run_compaction(self):
         with ThreadPoolExecutor(max_workers=self.config.parallelism) as executor:
             futures = []
-            for catalog in self.__get_catalogs():
-                logger.info(f"[{catalog}] Intorspecting catalog")
-                for database in self.__get_databases(catalog):
-                    db_name = database.namespace
-                    logger.info(f"[{db_name}] Intorspecting database")
-                    tables = self.spark.sql(f"show tables from {catalog}.{db_name}").collect()
-                    for table in tables:
-                        futures.append(executor.submit(self.__process_table_if_iceberg, catalog, db_name, table))
+            catalog  = self.__get_catalog()
+            logger.info(f"Starting table optimisation for catalog: {catalog}")
+
+            databases = self.__get_databases(catalog)
+            logger.info(f"Databases in catalog '{catalog}' considered for optimisation : {databases}")
+
+            db_table_mapping = defaultdict(list)
+            for database in databases:
+                logger.info(f"Introspecting database: {database}")
+                tables = self.__get_tables(catalog, database)
+                logger.info(f"Tables in database '{database}' considered for optimisation : {tables}")
+                if tables:
+                    db_table_mapping[database] = tables
+
+            init_emitter(self.spark)
+            for database in databases:
+                for table in db_table_mapping[database]:
+                    futures.append(executor.submit(self.__process_table_if_iceberg, catalog, database, table))
 
             for future in as_completed(futures):
                 try:
@@ -39,56 +50,167 @@ class SqlCompaction:
                     logger.error(f"Error processing table, error={e}")
 
     def __process_table_if_iceberg(self, catalog, db_name, table):
-        table_name = table.tableName
         try:
-            tableMeta = self.spark.sql(f"describe extended {db_name}.{table_name}").collect()
+            table_meta = self.spark.sql(f"describe extended {db_name}.{table}").collect()
 
             # Skip, if not an `iceberg` table
-            if not any(row.col_name == "Provider" and row.data_type == "iceberg" for row in tableMeta):
+            if not any(row.col_name == "Provider" and row.data_type == "iceberg" for row in table_meta):
                 return
 
-            message = f"[{db_name}.{table_name}] table compaction"
-            timer(message)(self.__process_table)(catalog, db_name, table_name)
+            message = f"[{db_name}.{table}] table compaction"
+            timer(message)(self.__process_table)(catalog, db_name, table)
 
         except Exception as e:
-            logger.error(f"[{db_name}.{table_name}] Error processing table, error={e}")
+            logger.error(f"[{db_name}.{table}] Error processing table, error={e}")
 
     def __process_table(self, catalog, database, table_name):
-        self.__expire_snapshots(catalog, database, table_name, self.config.expire_snapshot)
-        self.__remove_orphan_files(catalog, database, table_name, self.config.remove_orphan_files)
-        self.__rewrite_data_files(catalog, database, table_name, self.config.rewrite_data_files)
-        self.__rewrite_manifest(catalog, database, table_name, self.config.rewrite_manifests)
+        self.__expire_snapshots(catalog, database, table_name)
+        self.__remove_orphan_files(catalog, database, table_name)
+        self.__rewrite_data_files(catalog, database, table_name)
+        self.__rewrite_manifest(catalog, database, table_name)
 
-    def __expire_snapshots(self, catalog, database, table_name, expire_snapshot_config: ExpireSnapshotConfig):
-        options = f"table => '{database}.{table_name}', retain_last => {expire_snapshot_config.retain_last}"
-        self.spark.sql(f"CALL {catalog}.system.expire_snapshots({options})")
+    @emit_stats("EXPIRE_SNAPSHOTS")
+    def __expire_snapshots(self, catalog, database, table_name):
+        timestamp = datetime.now() - timedelta(minutes=5)
+        retain_last =int(self.__get_final_config_for_table(database,
+                                                           table_name,
+                                                           "expire_snapshot",
+                                                           "retain_last")
+                         or self.config.expire_snapshot.retain_last)
+        options = (f"table => '{database}.{table_name}',"
+                   f" retain_last => {retain_last},"
+                   f" older_than => TIMESTAMP '{timestamp}'")
+        query = f"CALL {catalog}.system.expire_snapshots({options})"
+        result = self.spark.sql(query).collect()
+        return result, query
 
-    def __remove_orphan_files(self, catalog, database, table_name, remove_orphan_files_config: RemoveOrphanFilesConfig):
+    @emit_stats("REMOVE_ORPHAN_FILES")
+    def __remove_orphan_files(self, catalog, database, table_name):
+        days = int(self.__get_final_config_for_table(database,
+                                                     table_name,
+                                                     "remove_orphan_files",
+                                                     "older_than_days")
+                   or self.config.remove_orphan_files.older_than_days)
+        timestamp = datetime.now(timezone.utc) - timedelta(days=days)
+        options = f"table => '{database}.{table_name}', older_than => TIMESTAMP '{timestamp}'"
+        query = f"CALL {catalog}.system.remove_orphan_files({options})"
+        result = self.spark.sql(query).collect()
+        return result, query
+
+    @emit_stats("REWRITE_MANIFESTS")
+    def __rewrite_manifest(self, catalog, database, table_name):
         options = f"table => '{database}.{table_name}'"
-        self.spark.sql(f"CALL {catalog}.system.remove_orphan_files({options})")
+        use_caching = (self.__get_final_config_for_table(database,
+                                                     table_name,
+                                                     "rewrite_manifest",
+                                                     "use_caching")
+                    or self.config.rewrite_manifests.use_caching)
+        if use_caching:
+            use_caching = str(use_caching).lower()
+            options += f", use_caching => {use_caching}"
+        query = f"CALL {catalog}.system.rewrite_manifests({options})"
+        result = self.spark.sql(query).collect()
+        return result, query
 
-    def __rewrite_manifest(self, catalog, database, table_name, rewrite_manifests_config: RewriteManifestsConfig):
+    @emit_stats("REWRITE_DATA_FILES")
+    def __rewrite_data_files(self, catalog, database, table_name):
+        strategy = (self.__get_final_config_for_table(database,
+                                                     table_name,
+                                                     "rewrite_data_files",
+                                                     "strategy")
+                    or self.config.rewrite_data_files.strategy)
+        sort_order = (self.__get_final_config_for_table(database,
+                                                     table_name,
+                                                     "rewrite_data_files",
+                                                     "sort_order")
+                    or self.config.rewrite_data_files.sort_order)
+        rewrite_options = (self.__get_final_config_for_table(database,
+                                                     table_name,
+                                                     "rewrite_data_files",
+                                                     "options")
+                    or self.config.rewrite_data_files.options)
+        where = (self.__get_final_config_for_table(database,
+                                                     table_name,
+                                                     "rewrite_data_files",
+                                                     "where")
+                    or self.config.rewrite_data_files.where)
+
         options = f"table => '{database}.{table_name}'"
-        if rewrite_manifests_config.use_caching is not None:
-            options += f", use_caching => {rewrite_manifests_config.use_caching}"
 
-        self.spark.sql(f"CALL {catalog}.system.rewrite_manifests({options})")
+        if strategy and strategy == "sort":
+            options += f", strategy => {strategy}, sort_order => {sort_order}"
+        if rewrite_options:
+            option_map = ', '.join(', '.join((f"'{k}'", f"'{v}'")) for (k, v) in rewrite_options.items())
+            options += f", options => map({option_map})"
+        if where:
+            options += f", where => {where}"
 
-    def __rewrite_data_files(self, catalog, database, table_name, rewrite_data_files_config: RewriteDataFilesConfig):
-        options = f"table => '{database}.{table_name}'"
-        if rewrite_data_files_config.options:
-            map = ', '.join(', '.join((f"'{k}'", f"'{v}'")) for (k, v) in rewrite_data_files_config.options.items())
-            options += f", options => map({map})"
+        query = f"CALL {catalog}.system.rewrite_data_files({options})"
+        result = self.spark.sql(query).collect()
+        return result, query
 
-        self.spark.sql(f"CALL {catalog}.system.rewrite_data_files({options})")
-
-    def __get_catalogs(self):
+    def __get_catalog(self):
+        catalog = self.config.catalog
         sql_client = SqlClient()
-        return sql_client.catalogs()
+        available_catalogs = sql_client.catalogs()
+        if catalog not in available_catalogs:
+            logger.error(f"Catalog not found: {catalog}. Available catalogs: {available_catalogs}.")
+            raise Exception(f"Catalog not found for optimisation: {catalog}")
+        return catalog
 
     def __get_databases(self, catalog):
-        databases = self.spark.sql(f"show databases from {catalog}").collect()
-        return databases
+        available_databases = [database.namespace for database
+                               in self.spark.sql(f"show databases from {catalog}").collect()]
+        if self.config.include_exclude.databases:
+            return [database for database in self.config.include_exclude.databases if database in available_databases]
+        else:
+            return available_databases
+
+    def __get_tables(self, catalog, database):
+        available_tables = [table.tableName for table
+                            in self.spark.sql(f"show tables from {catalog}.{database}").collect()]
+        if database in self.__get_table_includes():
+            tables = [table for table
+                      in self.__get_table_includes()[database]
+                      if table in available_tables]
+        elif database in self.__get_table_excludes():
+            tables = [table for table
+                      in available_tables
+                      if table not in self.__get_table_excludes()[database]]
+        else:
+            tables = available_tables
+        return tables
+
+    @cache
+    def __get_table_excludes(self):
+        mapping = defaultdict(list)
+        for table in self.config.include_exclude.table_exclude:
+            table_split = table.split('.')
+            if len(table_split) != 2:
+                logger.warning(f"Please provide table in format <database>.<table> instead of {table}")
+            else:
+                mapping[table_split[0]].append(table_split[1])
+        return mapping
+
+    @cache
+    def __get_table_includes(self):
+        mapping = defaultdict(list)
+        for table in self.config.include_exclude.table_include:
+            table_split = table.split('.')
+            if len(table_split) != 2:
+                logger.warning(f"Please provide table in format <database>.<table> instead of {table}")
+            else:
+                mapping[table_split[0]].append(table_split[1])
+        return mapping
+
+    def __get_final_config_for_table(self, database, table, operation, config_name):
+        if (self.config.table_overrides
+                and f"{database}.{table}" in self.config.table_overrides
+                and operation in self.config.table_overrides.get(f"{database}.{table}")
+                and config_name in self.config.table_overrides[f"{database}.{table}"][operation]):
+            return self.config.table_overrides[f"{database}.{table}"][operation][config_name]
+        else:
+            return None
 
 
 def timer(message: str):
---
+++ data_compaction_job/stats_emitter.py
@@ -0,0 +1,86 @@
+import time
+from datetime import datetime, timezone
+from functools import wraps
+
+from pyspark.sql import SparkSession
+
+
+def emit_stats(operation: str):
+    """Emits metric for the table optimisation operation that is being performed."""
+
+    def decorator(func):
+
+        @wraps(func)
+        def wrapper_func(*args, **kwargs):
+            #pre-execute
+            start_time = time.time()
+
+            #execute
+            try:
+                metrics, sql = func(*args, **kwargs)
+
+            except Exception as e:
+                #post-execute error scenario
+                end_time = time.time()
+                spark: SparkSession = args[0].spark
+                spark.sql(f"""INSERT INTO spark_catalog.system_db.table_optimisation_run_errors VALUES (
+                      '{spark.sparkContext.applicationId}',
+                      '{args[1]}',
+                      '{args[2]}',
+                      '{args[3]}',
+                      '{operation}',
+                      '{str(e)}',
+                      TIMESTAMP '{datetime.fromtimestamp(start_time ,timezone.utc).strftime('%Y-%m-%d %H:%M:%S')}',
+                      TIMESTAMP '{datetime.fromtimestamp(end_time, timezone.utc).strftime('%Y-%m-%d %H:%M:%S')}'
+                    )""")
+                raise e
+            else:
+                #post-execute happy scenario
+                end_time = time.time()
+
+                if operation == "REMOVE_ORPHAN_FILES":
+                    metric_map = f"'removed file count', '{len(metrics)}'"
+                else:
+                    metric_map = ", ".join([f"'{key}', '{value}'" for key, value in metrics[0].asDict().items()])
+
+                spark: SparkSession = args[0].spark
+                sql = sql.replace("'", "''")
+                spark.sql(f"""INSERT INTO spark_catalog.system_db.table_optimisation_run_metrics VALUES (
+      '{spark.sparkContext.applicationId}',
+      '{args[1]}',
+      '{args[2]}',
+      '{args[3]}',
+      '{operation}',
+      '{sql}',
+      MAP({metric_map}),
+      TIMESTAMP '{datetime.fromtimestamp(start_time, timezone.utc).strftime('%Y-%m-%d %H:%M:%S')}',
+      TIMESTAMP '{datetime.fromtimestamp(end_time, timezone.utc).strftime('%Y-%m-%d %H:%M:%S')}'
+    )""")
+
+        return wrapper_func
+
+    return decorator
+
+def init_emitter(spark: SparkSession):
+    spark.sql("create DATABASE IF NOT EXISTS spark_catalog.system_db")
+    spark.sql("""create TABLE IF NOT EXISTS spark_catalog.system_db.table_optimisation_run_metrics (
+  spark_app_id VARCHAR(255),
+  catalog_name VARCHAR(255),
+  database_name VARCHAR(255),
+  table_name VARCHAR(255),
+  operation VARCHAR(50),
+  query STRING,
+  metrics MAP<STRING, STRING>,
+  start_time TIMESTAMP,
+  end_time TIMESTAMP
+  )""")
+    spark.sql("""create TABLE IF NOT EXISTS spark_catalog.system_db.table_optimisation_run_errors(
+  spark_app_id VARCHAR(255),
+  catalog_name VARCHAR(255),
+  database_name VARCHAR(255),
+  table_name VARCHAR(255),
+  operation VARCHAR(50),
+  error STRING,
+  start_time TIMESTAMP,
+  end_time TIMESTAMP
+  )""")
